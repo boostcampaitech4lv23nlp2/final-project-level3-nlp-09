4d258b95b56396b046fdf19719e6f17217e9b90c
jinmyeongAN <jinmyeong.an@gmail.com>
2023-02-01 03:04:15 +0000

    fix   : change naming and commit id function

--- app/dev_app.py
+++ app/dev_app.py
@@ -5,19 +5,21 @@ import streamlit as st
 
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
-from utils import ModelWeakness, get_artifact, get_model_artifact_options, get_model_options, get_wandb_runs_df
+from utils import ModelWeakness, get_artifact, get_artifact_options, get_commit_id, get_model_options, get_wandb_runs_df
 
 runs_df = get_wandb_runs_df()
 model_option_list = get_model_options(runs_df)
 model_option = st.selectbox("Select your model ✅", model_option_list)
+commit_id = st.write("commit id: ", get_commit_id(runs_df, model_option))
 
-model_artifact_option_list = get_model_artifact_options(model_option, runs_df)
-model_artifact_option = st.selectbox("Select your model artifact ✅", model_artifact_option_list)
+artifact_option_list = get_artifact_options(model_option, runs_df)
+artifact_option = st.selectbox("Select your model artifact ✅", artifact_option_list)
 
 download_button = st.button("Download artifact")
 
 if download_button:
-    artifact_path = get_artifact(model_artifact_option)
-    modelWeakness = ModelWeakness(model_artifact_option, artifact_path)
+    artifact_path = get_artifact(artifact_option)
+    modelWeakness = ModelWeakness(artifact_option)
     weakness_df = modelWeakness.get_model_weakness()
+    weakness_df = weakness_df.loc[weakness_df.pred_texts != weakness_df.correct_texts]
     st.dataframe(weakness_df)
--- app/utils.py
+++ app/utils.py
@@ -3,9 +3,9 @@ import json
 import os
 
 import pandas as pd
+import streamlit as st
 import torch
 import wandb
-import streamlit as st
 
 from src.dataset import FoodImageDataset
 from src.model import build_model
@@ -16,9 +16,8 @@ from src.utils import set_seed
 
 
 class ModelWeakness:
-    def __init__(self, model_artifact, artifact_path):
-        self.model_artifact = model_artifact
-        self.artifact_path = artifact_path
+    def __init__(self, artifact):
+        self.artifact = artifact
         self.configs = self.get_model_config()
         self.text_cfg = self.configs["text_cfg"]
         self.vision_cfg = self.configs["vision_cfg"]
@@ -30,7 +29,7 @@ class ModelWeakness:
             # This was a default in pytorch until 1.12
             torch.backends.cuda.matmul.allow_tf32 = True
         self.preprocess = self.get_preprocess(self.vision_cfg)
-        self.model = self.get_model(self.args, self.vision_cfg, self.text_cfg, self.model_artifact, self.artifact_path)
+        self.model = self.get_model(self.args, self.vision_cfg, self.text_cfg, self.artifact)
         self.test_dataset = self.get_test_dataset(self.args, self.preprocess)
         self.tokens_path = "./src/model_configs/tokens_by_length.json"
         self.tokenizer = self.get_tokenizer(self.tokens_path, self.configs)
@@ -94,21 +93,20 @@ class ModelWeakness:
     def get_preprocess(self, vision_cfg):
         return image_transform(vision_cfg["image_size"], is_train=True)
 
-
-    def get_model(self, args, vision_cfg, text_cfg, model_artifact, artifact_path):
+    def get_model(self, args, vision_cfg, text_cfg, artifact):
         # TODO: wandb artifact로 바꾸기
-        path = os.path.join("app/artifacts", model_artifact[: model_artifact.find(".pt") + 3])
+        path = os.path.join("app/artifacts", artifact[: artifact.find(".pt") + 3])
 
         model = build_model(vision_cfg, text_cfg)
         checkpoint = torch.load(path, map_location="cpu")
         model.load_state_dict(checkpoint["state_dict"])
         # if scaler is not None and 'scaler' in checkpoint:
         #    scaler.load_state_dict(checkpoint['scaler'])
-        print(f"=> from resuming checkpoint '{model_artifact}' ")
+        print(f"=> from resuming checkpoint '{artifact}' ")
         return model
 
     def get_test_dataset(self, args, preprocess):
-        return FoodImageDataset(args, preprocess, mode="test", ratio=0.0001)
+        return FoodImageDataset(args, preprocess, mode="test", ratio=0.01)
 
     def get_tokenizer(self, tokens_path, configs):
         return FoodTokenizer(tokens_path, configs=configs)
@@ -130,10 +128,11 @@ def get_model_options(runs_df):
     return model_option
 
 
-def get_model_artifact_options(model_option, runs_df):
+def get_artifact_options(model_option, runs_df):
     df = runs_df[runs_df["artifacts"].apply(len) > 0]
     artifact_option = df.loc[df["run_name"] == model_option]["artifacts"].tolist()[0]
     return artifact_option
+
 
 @st.cache
 def get_wandb_runs_df(entity: str = "ecl-mlstudy", project: str = "FOOD CLIP"):
@@ -172,3 +171,8 @@ def get_artifact(artifact_name, entity: str = "ecl-mlstudy", project: str = "FOO
     artifact = api.artifact(entity + "/" + project + "/" + artifact_name)
     artifact.download(root="app/artifacts")
     return artifact_name
+
+
+def get_commit_id(runs_df, model_option):
+    commit_id = runs_df.loc[runs_df["run_name"] == model_option]["commit_id"].iloc[0]
+    return commit_id
